4a8ba7f93ed14ad3df427a62ba45113b42e3221b
Vunb <nhubaovu@gmail.com>
2020-08-05 11:00:24 +0700

    upgrade deps

--- app.ts
+++ app.ts
@@ -1,22 +1,18 @@
 import { KitesFactory } from '@kites/core';
-import Express from '@kites/express';
-import Rest from '@kites/rest';
 import { TodoService } from './todo/todo.service';
 
+// register controllers
 import './todo/todo.controller';
 
 async function bootstrap() {
   const app = await KitesFactory
     .create({
       loadConfig: true,
-      discover: false,
+      discover: true,
       providers: [
         TodoService,
       ],
     })
-    // Remove the use extensions manually if discover set to true.
-    .use(Express())
-    .use(Rest())
     .init();
 
   app.logger.info(`Server started! Let's browse http://localhost:3000/api/todo`);
